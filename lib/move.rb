172280459e22b7ca26f5bf9620821ef0ccfdcc5f
Alan Boles <bolesa@my.lowercolumbia.edu>
2018-06-21 18:50:16 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -9,7 +9,8 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(input)
-  index = input.to_i - 1
+input.to_i - 1
+index = input - 1
 end
 
 def move(board, index, value = "X")
